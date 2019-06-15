afd10639c40c8cb44c394ca7d6ce5326b252dbb1
nbelvoix <belvoixnicolas1997@gmail.com>
2019-06-15 09:30:49 +0200

    update général

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+
+        switch ($page) {
+            case 'index':
+                header('Location: controller/acceuil.php');
+                exit();
+                break;
+
+            case 'menu':
+                header('Location: controller/menu.php');
+                exit();
+                break;
+            
+            default:
+                header('Location: controller/acceuil.php');
+                exit();
+                break;
+        }
+    } else {
+        header('Location: controller/acceuil.php');
+        exit();
+    }
+    
+?>
